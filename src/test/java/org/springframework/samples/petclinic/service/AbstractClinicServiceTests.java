c770f0821e90e945345dcb1f288a6e4d4e77c34e
jmstechhome13 <96036369+jmstechhome13@users.noreply.github.com>
2021-12-29 09:50:01 +0530

    Update AbstractClinicServiceTests.java

--- src/test/java/org/springframework/samples/petclinic/service/AbstractClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/AbstractClinicServiceTests.java
@@ -170,8 +170,8 @@ public abstract class AbstractClinicServiceTests {
         Vet vet = EntityUtils.getById(vets, Vet.class, 3);
         assertThat(vet.getLastName()).isEqualTo("Douglas");
         assertThat(vet.getNrOfSpecialties()).isEqualTo(2);
-        assertThat(vet.getSpecialties().get(0).getName()).isEqualTo("dentistry");
-        assertThat(vet.getSpecialties().get(1).getName()).isEqualTo("surgery");
+        assertThat(vet.getSpecialties().get(0).getName()).isEqualTo("dentistryhjjbjhbjhvjhvj");
+        assertThat(vet.getSpecialties().get(1).getName()).isEqualTo("surgerynvnbvnbvnbnbbn");
     }
 
     @Test
